38be6edc4ab3d5f8fe62e0c543c3dc27f404fbbc
anuranjan <singh.anuranjan3@gmail.com>
2021-12-29 02:10:48 +0530

    Added static files in 'store'

--- index.js
+++ index.js
@@ -10,6 +10,7 @@ app.set('view engine','ejs');
 app.set('views',path.join(__dirname,'views'));
 app.use(express.urlencoded({extended:true}));
 app.use(express.json());
+app.use(express.static('store'));
 
 var contacts=[
     {
