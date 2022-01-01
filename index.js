2615762014fb39a212090f8fc2dc68bec32c1cf6
anuranjan <singh.anuranjan3@gmail.com>
2022-01-02 00:56:17 +0530

    Added deletion of contact logic with MongoDb

--- index.js
+++ index.js
@@ -66,14 +66,16 @@ app.post('/create-action',function(req,res){
 });
 // using query param to delete contact
 app.get('/delete-contact',function(req,res){
-    let phone=req.query.phone;
-
-    let contatIndex=contacts.findIndex(contact => contact.number==phone);
-    if (contatIndex!=-1){
-        contacts.splice(contatIndex,1);
-    }
-    return res.redirect('back');
-})
+    let id=req.query.id;
+    Contact.findOneAndDelete(id,function(err){
+        if (err){
+            console.log('error in deleting conatcts');
+            return;
+        }
+        return res.redirect('back');
+    });
+    
+});
 
 app.listen(port,function(err){
     if(err){
